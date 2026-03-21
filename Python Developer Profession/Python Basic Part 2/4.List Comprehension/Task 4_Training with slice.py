a517e7ef8591c754e8ca6bcccdb072d94ab32e2b
vesele_moloko <pikin.design@gmail.com>
2026-03-21 22:28:40 +0200

    Completed

--- Python Developer Profession/Python Basic Part 2/4.List Comprehension/Task 4_Training with slice.py
+++ Python Developer Profession/Python Basic Part 2/4.List Comprehension/Task 4_Training with slice.py
@@ -46,3 +46,15 @@ alphabet = 'abcdefg'
 Формат вывода соответствует примеру.
 Для получения результата используются только print и срезы переменной alphabet.
 """
+alphabet = 'abcdefg'
+
+print(alphabet[:])
+print(alphabet[::-1])
+print(alphabet[::2])
+print(alphabet[1::2])
+print(alphabet[:1])
+print(alphabet[-1:])
+print(alphabet[3])
+print(alphabet[4:])
+print(alphabet[3:5])
+print(alphabet[4:2:-1])
